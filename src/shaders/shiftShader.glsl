68b4795d7e1c2e986ccd0138d928ac57d7087bdc
Niklas Jurij Plessing <contact@niklasplessing.net>
2021-12-06 23:07:07 +0100

    a

--- src/shaders/shiftShader.glsl
+++ src/shaders/shiftShader.glsl
@@ -23,16 +23,16 @@ void main() {
 
   vec2 vUvNormal = vec2( vUv - 0.5 );
   vec2 vUvMouse = vec2( vUv.x - u_mouse.x, vUv.y - u_mouse.y);
-  vec2 dxy = pixelSize * 2./ resolution;
+  vec2 dxy = pixelSize * 10./ resolution;
   float circle = smoothstep(
-    0.8,
-    1.,
-    dot(vUvNormal, vUvNormal) * 4.
+    0.2,
+    1.2,
+    dot(vUvNormal, vUvNormal) * 2.
   );
   vec2 pixelated = (dxy * floor( vUv / dxy ) );
   // gl_FragColor = texture2D(tDiffuse, pixelated - circle);
   gl_FragColor = texture2D( tDiffuse, vUv + vUvNormal / 4.); // add border effect lines
   gl_FragColor = texture2D( tDiffuse, vUv + (1. - circle) * vUvNormal / 4.); // dont distort center with circle
-  gl_FragColor = texture2D( tDiffuse, vUv + ( pixelated - 0.5 )* ( circle ) * u_mouseSpeed * 3.5 ); //somehow working
+  gl_FragColor = texture2D( tDiffuse, vUv + ( pixelated - 0.5 )* ( circle ) * ( 1. - length(u_mouseSpeed) * 3.5 ) ); //somehow working
   // gl_FragColor = vec4( circle, circle, circle, 1.0 );
 }
